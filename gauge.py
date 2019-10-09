d0adab1b59d59bce4dc919ac344bee96c0d6f440
trytensorflow <43812306+trytensorflow@users.noreply.github.com>
2019-10-09 14:19:06 +0200

    float layout

--- gauge.py
+++ gauge.py
@@ -10,30 +10,34 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 import time
 
-class layout(Widget):
+class layout(FloatLayout):
 
     def __init__(self, **kwargs):
         super(layout, self).__init__(**kwargs)
-        self.widgetSize=(600,600)
-        self.cadran=Image(source="cadran.png",size=self.widgetSize)
+        self.widgetSize=Window.size
+        self.cadran=Image(source="cadran.png")
         self.add_widget(self.cadran)
         
-        
-        self.needle = Scatter(
+        self.container = Scatter(
             size=self.widgetSize,
-            #do_rotation=False,
-            #do_scale=False,
-            #do_translation=False
+            do_rotation=False,
+            do_scale=False,
+            do_translation=False,
             )
-        self.needle.add_widget(Image(source="needle.png",size=self.widgetSize))
-        self.add_widget(self.needle)
+        self.needle=Image(source="needle.png",size=self.widgetSize)
+        self.container.add_widget(self.needle)
+        self.add_widget(self.container)
        
         Clock.schedule_interval(self.move,1)
     def move(self,dt):
-        self.needle.center=self.cadran.center
+        print(self.container.center,self.cadran.center,self.needle.center)
+        # self.container.center=self.cadran.center
+        #self.container.rotation=45
+        self.container.pos_hint={"center":(.5,.5)}
         self.rotateNeedle(int(time.time())%21*5)
     def rotateNeedle(self,speed):
         """rotates the value to something between minValue (mapped to 0°) to maxValue (mapped to 180°)"""
@@ -41,12 +45,13 @@ class layout(Widget):
         maxValue=100
         if speed<minValue or speed>maxValue:
             raise ValueError("can only display values between {} and {}",minValue, maxValue)
-        self.needle.rotation=speed/(maxValue-minValue)*180-90
+        self.container.size=Window.size
+        self.container.rotation=speed/(maxValue-minValue)*180-90
         
 class gauge(App):
 
     def build(self):
-
+        # Window.fullscreen=True
         return layout()
 
 gauge().run()
